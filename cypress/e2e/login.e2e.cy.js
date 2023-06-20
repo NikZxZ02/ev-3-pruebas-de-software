c73266473e83e3520e327ff331533c01504f2802
NICOLÁS HIDALGO <n.hidalgo02@ufromail.cl>
2023-06-20 02:12:12 -0400

    Se agregan test de las diferentes vistas

--- cypress/e2e/login.e2e.cy.js
+++ cypress/e2e/login.e2e.cy.js
@@ -1,6 +1,6 @@
 describe("Login E2E", () => {
 	it("Login incorrect", () => {
-		cy.visit("http://3.89.221.7/login");
+		cy.visit(`${Cypress.config().baseUrl}/login`);
 		cy.get('input[id="login-email"]').type("john@example.com");
 		cy.get('input[id="login-password"]').type("john@example.com");
 		cy.get('button[id="login-submit"]').click();
@@ -12,7 +12,7 @@ describe("Login E2E", () => {
 	});
 
 	it("Login incorrect by Email", () => {
-		cy.visit("http://3.89.221.7/login");
+		cy.visit(`${Cypress.config().baseUrl}/login`);
 		cy.get('input[id="login-email"]').type("nicolas@gmail.c");
 		cy.get('input[id="login-password"]').type("2aSsword95%");
 		cy.get('button[id="login-submit"]').click();
@@ -25,13 +25,29 @@ describe("Login E2E", () => {
 
 
 	it("Correct login", () => {
-		cy.visit("http://3.89.221.7/login");
+		cy.visit(`${Cypress.config().baseUrl}/login`);
 		cy.get('input[type="email"]').type("john@example.com");
 		cy.get('input[type="password"]').type("2aSsword95%");
 		cy.get('button[type="submit"]').click();
 
 		cy.on("url:changed", () => {
 			cy.url().should("eq", `${Cypress.config().baseUrl}/`);
+		});
+	});
+
+	it("Correct login by email not verified", () => {
+		cy.visit("http://3.89.221.7/login");
+		cy.get('input[type="email"]').type("nicokashidalgo@gmail.com");
+		cy.get('input[type="password"]').type("2aSsword95%");
+		cy.get('button[id="login-submit"]').click();
+
+		cy.get("div")
+			.find(".q-notification")
+			.should("be.visible")
+			.and("contain.text", "You need to verify your account");
+
+		cy.on("url:changed", () => {
+			cy.url().should("eq", `${Cypress.config().baseUrl}/verify?verify_error=1`);
 		});
 	});
 });
