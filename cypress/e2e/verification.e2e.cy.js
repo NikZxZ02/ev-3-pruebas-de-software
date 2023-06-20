4893fe04f9db21657084d414ced7e540982e5eaf
Nicolas Pereira <nicopv.dev@gmail.com>
2023-06-20 19:10:11 -0400

    correcion test de verificacion

--- cypress/e2e/verification.e2e.cy.js
+++ cypress/e2e/verification.e2e.cy.js
@@ -1,5 +1,5 @@
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTIxNjZlOTFjMWNjNzRlYmQ3N2ZiYiIsImlhdCI6MTY4NzI5NTYxNiwiZXhwIjoxNjg3MzgyMDE2fQ.RvEPZf8ej0DOikuGEezWCBm2HF7w16cOKDMVmAKN4NE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTIzMTgwOTFjMWNjNzRlYmQ3ODJjZCIsImlhdCI6MTY4NzMwMjUzOCwiZXhwIjoxNjg3Mzg4OTM4fQ.n9jovDEsczdyqJVjMbJuyOGBFGHYPvldO7sbeiioKgc";
 
 const URL = Cypress.config().baseUrl;
 
@@ -29,7 +29,7 @@ describe("Verification E2E", () => {
     cy.get('button[type="submit"]').click();
     cy.contains("Verification successful").should("be.visible");
 
-    cy.url().should("eq", `${URL}`);
+    cy.url().should("eq", `${URL}/`);
   });
 
   it("Verification not necessary", () => {
@@ -43,6 +43,6 @@ describe("Verification E2E", () => {
     cy.get('input[id="verify-code"]').type("asd123");
     cy.get('button[type="submit"]').click();
     cy.contains("the user is already verified").should("be.visible");
-    cy.url().should("eq", `${URL}`);
+    cy.url().should("eq", `${URL}/`);
   });
 });
