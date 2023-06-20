63443e56db858bcf70b1327cd00897e950ce061a
Nicolas Pereira <nicopv.dev@gmail.com>
2023-06-20 17:27:16 -0400

    run test workflow en master

--- cypress/e2e/register.e2e.cy.js
+++ cypress/e2e/register.e2e.cy.js
@@ -1,9 +1,9 @@
 describe("Register E2E", () => {
   it("Correct register", () => {
     cy.visit("http://3.89.221.7/register");
-    cy.get('input[id="register-email"]').type("n.pereira01@ufromail.cl");
+    cy.get('input[id="register-email"]').type("test1@gmail.com");
     cy.get('input[id="register-name"]').type("Nicolas Pereira");
-    cy.get('input[id="register-rut"]').type("11819333-4");
+    cy.get('input[id="register-rut"]').type("8585889-0");
     cy.get('input[id="register-password"]').first().type("2aSsword95%");
     cy.get('input[id="register-password"]').eq(1).type("2aSsword95%");
     cy.get('button[type="submit"]').click();
